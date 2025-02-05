5a4fa815f98b8c2d8493a12dbf742ee6d36c7ea0
Nyle <nylewagjiani@gmail.com>
2025-02-05 18:02:24 +0000

    added licsense

--- Script.js
+++ Script.js
@@ -1,72 +1,72 @@
-const counterElement = document.getElementById("counter");
-const scoreElement = document.getElementById("score");
-const winPercentageElement = document.getElementById("win-percentage");
-const messageElement = document.getElementById("message");
-let countdown = 3;
-let startTime;
-let gameStarted = false;
-let successfulAttempts = 0;
-let totalAttempts = 0;
+const CounterElement = document.getElementById("Counter");
+const ScoreElement = document.getElementById("Score");
+const WinPercentageElement = document.getElementById("WinPercentage");
+const MessageElement = document.getElementById("Message");
+let Countdown = 3;
+let StartTime;
+let GameStarted = false;
+let SuccessfulAttempts = 0;
+let TotalAttempts = 0;
 
 document.body.addEventListener("click", () => {
-  if (gameStarted) {
-    const currentTime = new Date().getTime();
-    const elapsedTime = currentTime - startTime;
+  if (GameStarted) {
+    const CurrentTime = new Date().getTime();
+    const ElapsedTime = CurrentTime - StartTime;
 
-    if (elapsedTime <= 150) {
-      showMessage(`Success! You clicked in ${elapsedTime} milliseconds.`);
-      successfulAttempts++;
+    if (ElapsedTime <= 150) {
+      ShowMessage(`Success! You clicked in ${ElapsedTime} milliseconds.`);
+      SuccessfulAttempts++;
     } else {
-      showMessage(`You failed. You clicked after ${elapsedTime} milliseconds.`);
+      ShowMessage(`You failed. You clicked after ${ElapsedTime} milliseconds.`);
     }
 
-    totalAttempts++;
-    updateScore();
-    updateWinPercentage();
-    resetGame();
+    TotalAttempts++;
+    UpdateScore();
+    UpdateWinPercentage();
+    ResetGame();
   } else if (document.body.style.backgroundColor === "white") {
-    showMessage(`You failed. You clicked while background was white.`);
-    totalAttempts++;
-    updateScore();
-    updateWinPercentage();
-    resetGame();
+    ShowMessage(`You failed. You clicked while background was white.`);
+    TotalAttempts++;
+    UpdateScore();
+    UpdateWinPercentage();
+    ResetGame();
   }
 });
 
-function showMessage(message) {
-  messageElement.textContent = message;
-  messageElement.style.display = "block";
+function ShowMessage(Message) {
+  MessageElement.textContent = Message;
+  MessageElement.style.display = "block";
   setTimeout(() => {
-    messageElement.style.display = "none";
+    MessageElement.style.display = "none";
   }, 3000);
 }
 
-function updateScore() {
-  scoreElement.textContent = `Successful: ${successfulAttempts} / Total: ${totalAttempts}`;
+function UpdateScore() {
+  ScoreElement.textContent = `Successful: ${SuccessfulAttempts} / Total: ${TotalAttempts}`;
 }
 
-function updateWinPercentage() {
-  const winPercentage = (successfulAttempts / totalAttempts) * 100;
-  winPercentageElement.textContent = `Win Percentage: ${winPercentage.toFixed(
+function UpdateWinPercentage() {
+  const WinPercentage = (SuccessfulAttempts / TotalAttempts) * 100;
+  WinPercentageElement.textContent = `Win Percentage: ${WinPercentage.toFixed(
     2
   )}%`;
 }
 
-function startCountdown() {
-  counterElement.textContent = countdown;
-  if (countdown > 0) {
-    countdown--;
-    counterElement.style.color = "black"; // Set countdown color to green
+function StartCountdown() {
+  CounterElement.textContent = Countdown;
+  if (Countdown > 0) {
+    Countdown--;
+    CounterElement.style.color = "black"; // Set countdown color to green
     setTimeout(() => {
-      startCountdown();
+      StartCountdown();
     }, 1000);
   } else {
-    counterElement.textContent = "Click";
-    counterElement.style.color = "black"; // Reset countdown color
-    gameStarted = true;
+    CounterElement.textContent = "Click";
+    CounterElement.style.color = "black"; // Reset countdown color
+    GameStarted = true;
 
     // Capture the start time after countdown ends
-    startTime = new Date().getTime();
+    StartTime = new Date().getTime();
 
     // Change background color to red immediately upon clicking
     document.body.style.backgroundColor = "red";
@@ -74,13 +74,13 @@ function startCountdown() {
   }
 }
 
-function resetGame() {
-  countdown = 3; // Reset countdown time to fixed 3 seconds
-  gameStarted = false;
-  counterElement.textContent = "3";
+function ResetGame() {
+  Countdown = 3; // Reset countdown time to fixed 3 seconds
+  GameStarted = false;
+  CounterElement.textContent = "3";
   document.body.style.backgroundColor = "#00ff00"; // Reset background color
   document.body.style.cursor = "default";
-  setTimeout(startCountdown, 1000); // Start a new countdown after reset
+  setTimeout(StartCountdown, 1000); // Start a new countdown after reset
 }
 
-startCountdown();
+StartCountdown();
